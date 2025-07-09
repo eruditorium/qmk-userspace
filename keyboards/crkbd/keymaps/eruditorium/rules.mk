ccd9d603d67a8bd24761433e0fe542d92c156985
Nico <60152863+eruditorium@users.noreply.github.com>
2025-07-09 14:51:57 +0200

    Update rules.mk

--- keyboards/crkbd/keymaps/eruditorium/rules.mk
+++ keyboards/crkbd/keymaps/eruditorium/rules.mk
@@ -2,7 +2,7 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-OLED_ENABLE = no
+OLED_ENABLE = yes
 
 MOUSEKEY_ENABLE = no        # Mouse keys (+4700)
 CONSOLE_ENABLE = no         # Console for debug
